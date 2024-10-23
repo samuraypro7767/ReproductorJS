997c61738612625d6628fd79c4003c61f5d242d8
samuraypro7767 <sebaspro7767@gmail.com>
2024-10-23 07:34:10 -0500

    Images and audios were added, the js was also organized

---
+++ js/app.js
@@ -0,0 +1,19 @@
+import { playList } from "playList.js";
+
+const audio = document.getElementById('audio')
+const play = document.getElementById('play')
+const pause = document.getElementById('pause')
+const forward = document.getElementById('forward')
+const rewind = document.getElementById('rewind')
+const stop = document.getElementById('stop')
+
+
+play.addEventListener('click',() => audio.play())
+pause.addEventListener('click',() => audio.pause())
+rewind.addEventListener('click',() => audio.currentTime -= 10)
+forward.addEventListener('click',() => audio.currentTime += 10)
+stop.addEventListener('click',() => {
+    audio.pause()
+    audio.currentTime = 0
+})
+play.addEventListener('click',() => audio.play())
